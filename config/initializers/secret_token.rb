9ea3b1f34f2e7fb2a9a7d1907b5db5aab39178be
Ivan <ivan.ru@gmail.com>
2012-10-28 22:25:17 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smashingtwitter::Application.config.secret_token = '4cce3a8b435f0086ddfb147911f79efa6a7bb02d71d4ee64f3876c222fba5149097fdd7533d395f0bdcbaaa566ef7c6ffada783610f749f25a7eb2c47feec1e5'
